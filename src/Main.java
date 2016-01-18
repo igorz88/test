d90caafdde1e165fe44d0c3ecba60842e0df3f3d
igorz <wurezis@gmail.com>
2016-01-18 23:09:27 +0200

    tag

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World! (v-1.0)");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello World! (v-1.0 tag)");
+  }
 }
